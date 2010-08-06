70c5c73a44f0e3cc0f0303f4e48b66f8126b7e47
kenpu <kenpu@ubuntu.ubuntu-domain>
2010-08-06 10:18:53 -0400

    update

--- sandbox/streamrt/fileaccessor.py
+++ sandbox/streamrt/fileaccessor.py
@@ -1,6 +1,6 @@
 import os
 import itertools
-from datetime import datetime
+from datetime import datetime, timedelta
 import tarfile
 
 """
@@ -10,6 +10,11 @@ special timestamp handling.
 
 def getTarDatetime(filename):
   return datetime.strptime(filename[:20], "%Y-%m-%d__%H-%M-%S")
+
+def getTarTimespan(fullpath):
+  x = tarfile.open(fullpath)
+  members = x.getnames()
+  return (getImgDatetime(members[0]), getImgDatetime(members[-1]))
   
 def getImgDatetime(filename):
   return datetime.strptime(filename[:20], "%Y-%m-%d__%H:%M:%S")
@@ -20,7 +25,11 @@ def getImages(tarfilename, open=True):
   while not tarinfo == None:
     if open:
       fileobj = untarred.extractfile(tarinfo)
-  
+      yield (getImgDatetime(tarinfo.name), fileobj)
+    else:
+      yield getImgDatetime(tarinfo.name)
+    tarinfo = untarred.next()
+
 class ImageDirectory:
   """
   Allows random access of the image frames in the tar files in the
@@ -33,24 +42,54 @@ class ImageDirectory:
     self.files = []
     self.param = {}
     self.refresh()
-    self.file_pointer = self.files[0]
-    
+    self.time_pointer = None
     
   def refresh(self):
     self.files = sorted(x for x in os.listdir(self.path) if x.endswith(".tar"))
+    self.timespans = [getTarTimespan(os.path.join(self.path, x)) for x in self.files]
     if not self.files:
       raise Exception("%s does not contain any tar files")
-      
-    self.param['start_time'] = getTarDatetime(self.files[0])
-    x = tarfile.open(os.path.join(self.path, self.files[-1]))
-    members = x.getnames()
-    self.param['end_time'] = getImgDatetime(members[-1])
-    self.param['duration'] = (self.param['end_time'] - self.param['start_time'])
+
+    self.param['start_time'] = self.timespans[0][0]
+    self.param['end_time'] = self.timespans[-1][1]
+    self.param['step'] = timedelta(seconds=10)
   
   def durationInSeconds(self):
     return self.duration * 24 *3600 + self.seconds
   
   def setParameter(self, attr, val, restart=False):
     self.param[attr] = val
+      
+  def jumpTo(self, t):    
+    for (i, (start, end)) in enumerate(self.timespans):
+      if t <= end:
+        self.file_pointer = i
+        self.img_iterator = getImages(os.path.join(self.path, self.files[i]), open=True)
+        return start
     
-  def 
+  def getNextFrameInStream(self, next_time):
+    for (t, fileobj) in self.img_iterator:
+      if next_time <= t:
+        return t, len(fileobj.read())
+    raise Exception("next_time %s is not in stream" % str(next_time))
+    
+  def next(self):
+    
+    if self.time_pointer == None:
+      self.time_pointer = self.param['start_time']
+      self.jumpTo(self.time_pointer)
+      
+    next_time = self.time_pointer + self.param['step']
+    timespan = self.timespans[self.file_pointer]
+    if next_time >= self.param['end_time']:
+      self.time_pointer = self.param['start_time']
+      next_time = self.jumpTo(self.time_pointer)
+    elif not (timespan[0] <= next_time <= timespan[1]):
+      next_time = self.jumpTo(next_time)
+
+    frame = self.getNextFrameInStream(next_time)
+    self.time_pointer = frame[0]
+    return frame
+      
+  def __iter__(self):
+    return self
